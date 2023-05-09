994b0c452385a9d512dde05a59cab9898607a3ac
Whfugv <whfugv@123.com>
2023-05-09 18:51:12 +0800

    hot-fix commit

--- src/main/java/com/git/one/GitTest.java
+++ src/main/java/com/git/one/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello git2222!");
         System.out.println("Hello git333!");
+        System.out.println("hot-fix");
     }
 }
 
