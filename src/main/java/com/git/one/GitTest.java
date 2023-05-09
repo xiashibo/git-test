52afcc44f9f1d685bee08c33ef89987d5e170ef4
Whfugv <whfugv@123.com>
2023-05-09 20:58:01 +0800

    push commit

--- src/main/java/com/git/one/GitTest.java
+++ src/main/java/com/git/one/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
 
