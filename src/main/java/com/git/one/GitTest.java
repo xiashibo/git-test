1a382ba9abb49b8a01927c84268d95348d97b19c
Whfugv <whfugv@123.com>
2023-05-09 18:47:52 +0800

    first commit

---
+++ src/main/java/com/git/one/GitTest.java
@@ -0,0 +1,16 @@
+package com.git.one;
+
+/**
+ * [一句话描述该类的功能]
+ *
+ * @author : [Tom]
+ * @version : [v1.0]
+ * @createTime : [2023/5/9 18:44]
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
+
